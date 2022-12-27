139677e50c293e1de8c31f91d0b4f6c55c05600d
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-27 19:02:58 +0000

    feat: criando class CreateCategoryController

--- chapter-II-iniciando-a-API(typescript)/rentalx/src/routes/categories.routes.ts
+++ chapter-II-iniciando-a-API(typescript)/rentalx/src/routes/categories.routes.ts
@@ -6,13 +6,7 @@ const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
-  const { name, description } = req.body;
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
 });
 
 categoriesRoutes.get('/', (req, res) => {
